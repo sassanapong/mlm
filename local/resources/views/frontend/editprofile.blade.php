340a8e2e12a5e6375ec2b2b4b548fd14c370f670
nitipoom phapakdee <nitipoom42@gmail.com>
2022-08-30 17:05:59 +0700

    :bug: :package:

--- local/resources/views/frontend/editprofile.blade.php
+++ local/resources/views/frontend/editprofile.blade.php
@@ -213,8 +213,9 @@
                                 <div class="col-md-4">
 
                                     <div class="mt-1 mb-2 d-flex justify-content-center">
+
                                         <img width="250" height="300" id="img_card"
-                                            src="{{ $address_card->url . '/' . $address_card->img_card }}" />
+                                            src="'{{ $address_card->url . '/' . $address_card->img_card }}'" />
                                     </div>
 
                                 </div>
