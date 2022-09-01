925b89b3b2ff9115a2a2de7e3b044b510f44cb8a
nitipoom phapakdee <nitipoom42@gmail.com>
2022-09-01 15:58:02 +0700

    :sparkles: แก้เมนู

--- local/resources/views/backend/navbar/navbar.blade.php
+++ local/resources/views/backend/navbar/navbar.blade.php
@@ -1,12 +1,38 @@
 <nav class="side-nav">
     <a href="" class="intro-x flex items-center pl-5 pt-4 mt-3">
-        <img alt="Midone - HTML Admin Template" class="w-6" src="{{ asset('backend/dist/images/logo.svg') }}">
-        <span class="hidden xl:block text-white text-lg ml-3"> Tinker </span>
+        <img alt="Midone - HTML Admin Template" class="w-24 mx-auto" src="{{ asset('frontend/images/logo.png') }}">
+
     </a>
     <div class="side-nav__devider my-6"></div>
     <ul>
 
+        {{-- BEGIN ระบบสมาชิก --}}
+        <li>
+            <a href="javascript:;.html" class="side-menu ">
+                <div class="side-menu__icon"> <i data-lucide="users" class="menu__sub-icon"></i> </div>
+                <div class="side-menu__title">
+                    ระบบสมาชิก
+                    <div class="side-menu__sub-icon transform rotate-180"> <i data-lucide="chevron-down"></i> </div>
+                </div>
+            </a>
+            <ul class="side-menu__sub-icon ml-4">
+                <li>
+                    <a href="" class="side-menu ">
+                        <div class="side-menu__icon"> <i data-lucide="activity" class="menu__sub-icon "></i> </div>
+                        <div class="side-menu__title"> สมาชิก </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="side-menu">
+                        <div class="side-menu__icon"> <i data-lucide="activity" class="menu__sub-icon "></i> </div>
+                        <div class="side-menu__title"> กำหนดสิทธ์ (Role) </div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        {{-- END ระบบสมาชิก --}}
 
+        <li class="side-nav__devider my-6"></li>
         {{-- BEGIN จัดการสินค้า --}}
         <li>
             <a href="javascript:;.html" class="side-menu side-menu--active">
@@ -20,13 +46,13 @@
                 <li>
                     <a href="" class="side-menu side-menu--active">
                         <div class="side-menu__icon"> <i data-lucide="activity" class="menu__sub-icon "></i> </div>
-                        <div class="side-menu__title"> กลุ่มสินค้า </div>
+                        <div class="side-menu__title"> สินค้ารอจัดส่ง </div>
                     </a>
                 </li>
                 <li>
                     <a href="" class="side-menu">
                         <div class="side-menu__icon"> <i data-lucide="activity" class="menu__sub-icon "></i> </div>
-                        <div class="side-menu__title"> จัดการสินค้า </div>
+                        <div class="side-menu__title"> สถานะสินค้าจัดส่ง </div>
                     </a>
                 </li>
                 <li>
@@ -145,13 +171,13 @@
         {{-- END กระเป๋าเงิน --}}
         <li class="side-nav__devider my-6"></li>
 
-
         {{-- BEGIN จัดการผู้ใช้งาน --}}
         <li>
             <a href="javascript:;" class="side-menu ">
                 <div class="side-menu__icon"> <i data-lucide="user" class="menu__sub-icon "></i> </div>
                 <div class="side-menu__title">
                     จัดการผู้ใช้งาน
+
                 </div>
             </a>
         </li>
