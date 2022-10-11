8ba61bf7cab47c60ad3b380561991e4d73500bc6
nitipoom phapakdee <nitipoom42@gmail.com>
2022-10-11 16:02:07 +0700

    :bug:  join Customers

--- local/app/Http/Controllers/Backend/OrderController.php
+++ local/app/Http/Controllers/Backend/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Customers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
@@ -23,19 +24,20 @@ class OrderController extends Controller
 
         $orders = DB::table('db_orders')
 
-            ->select('db_orders.*', 'dataset_order_status.detail', 'dataset_order_status.css_class')
-            ->leftjoin('dataset_order_status', 'dataset_order_status.orderstatus_id', '=', 'db_orders.order_status_id_fk')
+            ->select(
+                'db_orders.*',
+                'dataset_order_status.detail',
+                'dataset_order_status.css_class',
 
+            )
+            ->leftjoin('dataset_order_status', 'dataset_order_status.orderstatus_id', '=', 'db_orders.order_status_id_fk')
+            ->leftjoin('customers', 'customers.id', '=', 'db_orders.customers_id_fk')
             ->where('dataset_order_status.lang_id', '=', 1)
             // ->where('db_orders.order_status_id_fk', ['2',])
             // ->whereRaw(("case WHEN '{$request->s_date}' != '' and '{$request->e_date}' = ''  THEN  date(db_orders.created_at) = '{$request->s_date}' else 1 END"))
             // ->whereRaw(("case WHEN '{$request->s_date}' != '' and '{$request->e_date}' != ''  THEN  date(db_orders.created_at) >= '{$request->s_date}' and date(db_orders.created_at) <= '{$request->e_date}'else 1 END"))
             // ->whereRaw(("case WHEN '{$request->s_date}' = '' and '{$request->e_date}' != ''  THEN  date(db_orders.created_at) = '{$request->e_date}' else 1 END"))
-
-            ->orderby('db_orders.updated_at', 'DESC')
-            ->get();
-
-
+            ->orderby('db_orders.updated_at', 'DESC');
 
         return DataTables::of($orders)
             ->setRowClass('intro-x py-4 h-20 zoom-in box ')
@@ -44,6 +46,12 @@ class OrderController extends Controller
                 $price = $query->total_price;
                 return  number_format($price, 2) . ' บาท';
             })
+
+            // รวม รหัสกับชื่อสมาชิก
+            // ->editColumn('customers_user_name', function ($query) {
+
+            //     return   $customers;
+            // })
             ->editColumn('created_at', function ($query) {
                 $time =  date('d-m-Y h:i', strtotime($query->created_at));
                 return   $time . ' น';
