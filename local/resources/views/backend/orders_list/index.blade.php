dcdaf5dc59cd06f33df07e38a7218dc947241278
therdsa <93250149+therdsa@users.noreply.github.com>
2022-10-18 09:07:55 +0700

    update

--- local/resources/views/backend/orders_list/index.blade.php
+++ local/resources/views/backend/orders_list/index.blade.php
@@ -44,7 +44,7 @@
                             <form action="{{ route('importorder') }}" method="post" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-inline ">
-                                    <input name="excel" type="file"
+                                    <input name="excel" type="file" required
                                         class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-green-50 file:text-green-700 hover:file:bg-green-100">
                                 </div>
                         </div>
