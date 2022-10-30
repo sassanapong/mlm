e059b8eb7fd5587c76cb4936d02d5ae277d673eb
therdsa <93250149+therdsa@users.noreply.github.com>
2022-10-30 17:53:54 +0700

    Update eWalletController.php

--- local/app/Http/Controllers/Frontend/eWalletController.php
+++ local/app/Http/Controllers/Frontend/eWalletController.php
@@ -123,7 +123,7 @@ class eWalletController extends Controller
             ->editColumn('customers_name_receive', function ($query) {
                 $customers = Customers::select('user_name','name', 'last_name')->where('id', $query->customers_id_receive)->first();
                 if($customers){
-                    $test_customers = $customers['name'] . " " . $customers['last_name'].' ('.$customers['user_name'].')' ;
+                    $test_customers = ' ('.$customers['user_name'].')' ;
                 }else{
                     $test_customers="-";
                 }
@@ -131,7 +131,7 @@ class eWalletController extends Controller
             })
             ->editColumn('customers_id_fk', function ($query) {
                 $customers = Customers::select('user_name','name', 'last_name')->where('id', $query->customers_id_fk)->first();
-                $test_customers = $customers['name'] . " " . $customers['last_name'].' ('.$customers['user_name'].')' ;
+                $test_customers = ' ('.$customers['user_name'].')' ;
                 return $test_customers;
             })
 
