e18be9c23df61181ff66c6f078a8cde98474516d
Sasanapong <sassanapong.p@gmail.com>
2022-11-15 17:41:02 +0700

    Runbonus

--- local/app/Http/Controllers/Frontend/FC/RunErrorController.php
+++ local/app/Http/Controllers/Frontend/FC/RunErrorController.php
@@ -55,6 +55,14 @@ class RunErrorController extends Controller
         //  $data = RunErrorController::run_edit_upline();
         // dd($data);
 
+           $data = RunErrorController::import_bonus_total();
+        dd($data);
+
+
+        // $data = RunErrorController::run_bonus_total();
+        // dd($data);
+
+
 
 
         // $group = DB::table('customers')
@@ -82,14 +90,184 @@ class RunErrorController extends Controller
         // dd($i,'success');
 
 
-        $data = \App\Http\Controllers\Frontend\BonusCopyrightController::RunBonus_copyright_3();
-        dd($data); 
+        // $data = \App\Http\Controllers\Frontend\BonusCopyrightController::RunBonus_copyright_3();
+        // dd($data);
         // dd($i,'success');
 
 
 
 
         // return view('frontend/jp-clarify');
+    }
+    public static function run_bonus_total()
+    {
+
+
+
+        /////////////////////// 3 ////////////////////////ลิขสิท รัน
+
+        // $report_bonus_cashback =  DB::table('report_bonus_copyright') //รายชื่อคนที่มีรายการแจงโบนัสข้อ
+        //     ->selectRaw('customer_user,sum(total_bonus) as total_bonus')
+        //     ->where('status', '=', 'success')
+        //     ->groupby('customer_user')
+        //     ->get();
+
+        // $i = 0;
+        // foreach ($report_bonus_cashback as $value) {
+        //     $i++;
+        //     $wallet_g = DB::table('customers')
+        //         ->select('bonus_total', 'user_name')
+        //         ->where('user_name', $value->customer_user)
+        //         ->first();
+        //     $bonus_total = $wallet_g->bonus_total  + $value->total_bonus;
+        //     DB::table('customers')
+        //         ->where('user_name', $value->customer_user)
+        //         ->update(['bonus_total' => $bonus_total]);
+        // }
+        // dd('success ' . $i . ' Total' . count($report_bonus_cashback));
+
+        /////////////////////// 3 ////////////////////////สนับสนุนสินค้า ได้รับ
+
+        // $jang_pv =  DB::table('jang_pv') //รายชื่อคนที่มีรายการแจงโบนัสข้อ
+        //     ->selectRaw('customer_username as customer_user,sum(wallet) as total_bonus')
+        //     ->where('type', '=', '1')
+        //     ->where('status', '=', 'success')
+        //     ->groupby('customer_username')
+        //     ->get();
+        //     // dd(count($jang_pv));
+
+        // $i = 0;
+        // foreach ($jang_pv as $value) {
+        //     $i++;
+        //     $wallet_g = DB::table('customers')
+        //         ->select('bonus_total', 'user_name')
+        //         ->where('user_name', $value->customer_user)
+        //         ->first();
+        //     $bonus_total = $wallet_g->bonus_total  + $value->total_bonus;
+        //     DB::table('customers')
+        //         ->where('user_name', $value->customer_user)
+        //         ->update(['bonus_total' => $bonus_total]);
+        // }
+        // dd('success ' . $i . ' Total' . count($jang_pv));
+
+        /////////////////////// 5 ////////////////////////
+
+        //      $db_orders =  DB::table('db_orders') //รายชื่อคนที่มีรายการแจงโบนัสข้อ
+        //      ->selectRaw('customers_user_name as customer_user,sum(discount) as total_bonus')
+        //     //  ->where('type', '=', '1')
+        //     //  ->where('status', '=', 'success')
+        //      ->groupby('customers_user_name')
+        //      ->get();
+        //      // dd(count($jang_pv));
+
+        //  $i = 0;
+        //  foreach ($db_orders as $value) {
+        //      $i++;
+        //      $wallet_g = DB::table('customers')
+        //          ->select('bonus_total', 'user_name')
+        //          ->where('user_name', $value->customer_user)
+        //          ->first();
+        //      $bonus_total = $wallet_g->bonus_total  + $value->total_bonus;
+        //      DB::table('customers')
+        //          ->where('user_name', $value->customer_user)
+        //          ->update(['bonus_total' => $bonus_total]);
+        //  }
+        //  dd('success ' . $i . ' Total' . count($db_orders));
+
+        $data_user_1 =  DB::table('customers')
+            ->select('customers.name', 'customers.last_name','bonus_total', 'customers.user_name', 'customers.upline_id', 'customers.qualification_id', 'customers.expire_date')
+            ->leftjoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
+            // ->where('user_name', '=', '0005064')
+            ->where('dataset_qualification.id', '=', 4)
+            ->get();
+        $i = 0;
+        $k = 0;
+        // dd($data_user_1);
+        //ขึ้น XVVIP แนะนำ 2 VVIP คะแนน 0ว
+        foreach ($data_user_1 as $value) {
+            $i++;
+            $data_user =  DB::table('customers')
+                ->leftjoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
+                ->where('customers.introduce_id', '=', $value->user_name)
+                ->where('dataset_qualification.id', '=', 4)
+                ->count();
+                // dd($data_user);
+            if ($data_user >= 200) { //MD
+                $data_svvip =  DB::table('customers')
+                    ->leftjoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
+                    ->where('customers.introduce_id', '=', $value->user_name)
+                    ->where('dataset_qualification.id', '=', 6)
+                    ->count();
+                if ($data_svvip >= 21) {
+                    if ($value->bonus_total >= 3000000) {
+                        $k++;
+                        DB::table('customers')
+                            ->where('user_name', $value->user_name)
+                            ->update(['qualification_id' => 'MD']);
+                        DB::table('log_up_vl')->insert(['user_name' => $value->user_name, 'old_lavel' => $data_user->code, 'new_lavel' => 'MD']);
+                    }
+                }
+            } elseif ($data_user >= 150) {
+                $data_svvip =  DB::table('customers')
+                    ->leftjoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
+                    ->where('customers.introduce_id', '=', $value->user_name)
+                    ->where('dataset_qualification.id', '=', 6)
+                    ->count();
+                if ($data_svvip >= 13) {
+                    if ($value->bonus_total >= 2000000) {
+                        $k++;
+                        DB::table('customers')
+                            ->where('user_name', $value->user_name)
+                            ->update(['qualification_id' => 'ME']);
+                        DB::table('log_up_vl')->insert(['user_name' => $value->user_name, 'old_lavel' => $value->qualification_id, 'new_lavel' => 'ME']);
+                    }
+                }
+            } elseif ($data_user >= 100) {
+                $data_svvip =  DB::table('customers')
+                    ->leftjoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
+                    ->where('customers.introduce_id', '=', $value->user_name)
+                    ->where('dataset_qualification.id', '=', 6)
+                    ->count();
+                if ($data_svvip >= 7) {
+                    if ($value->bonus_total >= 1000000) {
+                        $k++;
+                        DB::table('customers')
+                            ->where('user_name', $value->user_name)
+                            ->update(['qualification_id' => 'ME']);
+                        DB::table('log_up_vl')->insert(['user_name' => $value->user_name, 'old_lavel' => $value->qualification_id, 'new_lavel' => 'ME']);
+                    }
+                }
+            } elseif ($data_user >= 60) {
+                $data_svvip =  DB::table('customers')
+                    ->leftjoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
+                    ->where('customers.introduce_id', '=', $value->user_name)
+                    ->where('dataset_qualification.id', '=', 6)
+                    ->count();
+                if ($data_svvip >= 3) {
+                    if ($value->bonus_total >= 400000) {
+                        $k++;
+                        DB::table('customers')
+                            ->where('user_name', $value->user_name)
+                            ->update(['qualification_id' => 'MG']);
+                        DB::table('log_up_vl')->insert(['user_name' => $value->user_name, 'old_lavel' => $value->qualification_id, 'new_lavel' => 'MG']);
+                    }
+                }
+            }elseif ($data_user >= 40) {
+                $k++;
+
+                DB::table('customers')
+                    ->where('user_name', $value->user_name)
+                    ->update(['qualification_id' => 'SVVIP']);
+                DB::table('log_up_vl')->insert(['user_name' => $value->user_name, 'old_lavel' => $value->qualification_id, 'new_lavel' => 'SVVIP']);
+            } elseif ($data_user >= 2) {
+                $k++;
+                DB::table('customers')
+                    ->where('user_name', $value->user_name)
+                    ->update(['qualification_id' => 'XVVIP']);
+                DB::table('log_up_vl')->insert(['user_name' => $value->user_name, 'old_lavel' => $value->qualification_id, 'new_lavel' => 'XVVIP','bonus_total'=>$value->bonus_total]);
+            }
+        }
+        dd('success ' . $i . ' Total' . count($data_user_1) .'ปรับขึ้นตำแหน่ง'.$k);
     }
 
     public static function run_edit_upline()
@@ -102,7 +280,7 @@ class RunErrorController extends Controller
         foreach ($c as $value) {
             DB::table('customers')
                 ->where('user_name', $value->user_name)
-                ->update(['upline_id' => $value->upline,'type_upline'=> $value->type]);
+                ->update(['upline_id' => $value->upline, 'type_upline' => $value->type]);
             $i++;
         }
 
@@ -129,11 +307,11 @@ class RunErrorController extends Controller
     {
         $c = DB::table('customers')
             ->select('user_name', 'remain_date_num')
-            ->where('remain_date_num','>',0)
+            ->where('remain_date_num', '>', 0)
             ->get();
         $i = 0;
         foreach ($c as $value) {
-           $data = date('Y-m-d', strtotime("+ $value->remain_date_num days"));
+            $data = date('Y-m-d', strtotime("+ $value->remain_date_num days"));
 
             DB::table('customers')
                 ->where('user_name', $value->user_name)
@@ -204,6 +382,31 @@ class RunErrorController extends Controller
             ];
 
             $query =  eWallet::create($dataPrepare);
+
+            $i++;
+        }
+
+        dd($i, 'success');
+    }
+
+    public static function import_bonus_total()
+    {
+        $c = DB::table('excel_imort_bonus_total')
+            ->select('user_name', 'bonus_total')
+            ->limit(10)
+            ->get();
+        $i = 0;
+
+        foreach ($c as $value) {
+            $customers = DB::table('customers')
+                ->select('id', 'user_name', 'bonus_total')
+                ->where('user_name', $value->user_name)
+                ->first();
+
+            $bonus_total = $customers->bonus_total + $value->bonus_total;
+            DB::table('customers')
+                ->where('user_name', $value->user_name)
+                ->update(['bonus_total' => $bonus_total]);
 
             $i++;
         }
