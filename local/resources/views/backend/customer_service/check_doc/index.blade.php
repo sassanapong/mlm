14c283035374b51e1909e34bf258049932de6bf8
nitipoom phapakdee <nitipoom42@gmail.com>
2022-11-22 10:15:01 +0700

    :sparkles:  เข้าสู่ระบบจากการกรอกรหัสสมาชิก

--- local/resources/views/backend/customer_service/check_doc/index.blade.php
+++ local/resources/views/backend/customer_service/check_doc/index.blade.php
@@ -24,34 +24,20 @@
             <div class="grid grid-cols-12 gap-6 mt-5">
                 <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
                     <div class="">
+                        <label for="" class="mr-2 text-slate-500">แก้ไขข้อมูลจากรหัสสมาชิก</label>
                         <div class="form-inline ">
                             <form id="href_username" method="post">
                                 @csrf
-                                <input type="text" name="user_name" class="form-control w-56" placeholder="รหัสผู้ใช้">
-                                <button type="submit"
-                                    class="btn btn-success bg-green-700 text-white mr-5 btn-sm  ">ตกลง</button>
+                                <input type="text" name="user_name" class="form-control w-56 mt-2"
+                                    placeholder="รหัสผู้ใช้">
+                                <button type="submit" class="btn btn-success bg-green-700 text-white ml-2 btn-sm  ">
+                                    <i class="fa-solid fa-right-to-bracket mr-2"></i> ตกลง</button>
                             </form>
-
-                            <label for="" class="mr-2 text-slate-500 ">สถานะ : </label>
-                            <select class="w-32 form-select box mt-3 sm:mt-0">
-                                <option>ทั้งหมด</option>
-                                <option>ผ่าน</option>
-                                <option>ไม่ผ่าน</option>
-                            </select>
                         </div>
                     </div>
-
+                    _
                     <div class="hidden md:block mx-auto text-slate-500"></div>
                     <div class="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
-
-                        <div class=" relative text-slate-500">
-                            <div class="form-inline">
-                                <label for="" class="mr-2">ค้นหาจากรหัสผู้ใช้</label>
-                                <input type="text" class="form-control w-56 box pr-10 myWhere" name="user_name"
-                                    placeholder="Search...">
-                                <i class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0" data-lucide="search"></i>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <!-- BEGIN: Data List -->
@@ -122,8 +108,7 @@
                                 </div>
                                 <div class="col-span-4">
                                     <div> <label for="rtambon" class="form-label">ตำบล</label>
-                                        <input id="tambon" type="text" class="form-control" value=""
-                                            readonly>
+                                        <input id="tambon" type="text" class="form-control" value="" readonly>
                                     </div>
                                 </div>
                                 <div class="col-span-4">
