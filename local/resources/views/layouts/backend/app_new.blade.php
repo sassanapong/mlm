659bab3978d370403be63fc6700017bb927fa1f4
Sasanapong <sassanapong.p@gmail.com>
2023-06-12 17:27:34 +0700

    ssss

--- local/resources/views/layouts/backend/app_new.blade.php
+++ local/resources/views/layouts/backend/app_new.blade.php
@@ -4,12 +4,12 @@
     <!-- BEGIN: Head -->
     <head>
         <meta charset="utf-8">
-        <link href="{{ asset('backend/dist/images/logo.svg') }}" rel="shortcut icon">
+        <link href="{{ asset('frontend/images/fav-icon.png')}}" rel="shortcut icon">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Tinker admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
         <meta name="keywords" content="admin template, Tinker Admin Template, dashboard template, flat admin template, responsive admin template, web app">
         <meta name="author" content="LEFT4CODE">
-        <title>บริษัท มารวยด้วยกัน จำกัด</title>
+        <title>บริษัท มารวยด้วยกัน จำกัด </title>
         <link rel="stylesheet" href="{{ asset('backend/dist/css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('backend/dist/css/style.css') }}">
         {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css"> --}}
