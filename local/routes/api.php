e3ef9527f037c8ef66e7dc6f206ea6d00e52bb5b
sassanapong.p <sassanapong.p@gmail.com>
2024-04-25 16:40:29 +0700

    กกก

--- local/routes/api.php
+++ local/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('sss', 'Api\Customer@cart_delete')->name('sss');
+
+$id = Auth::guard('c_user')->user()->user_name;
+$intoken = date("ymd") . '' . $id . '' . date("H");
+$token = hash('SHA512', $intoken);
