8dd9768967a0ce0af082c44769a2364c9656269b
sassanapong.p <sassanapong.p@gmail.com>
2024-05-15 15:19:52 +0700

    หหห

--- local/app/Http/Middleware/AuthenticateJWT.php
+++ local/app/Http/Middleware/AuthenticateJWT.php
@@ -3,24 +3,25 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
-class AuthenticateJWT
+class AuthenticateJWT extends BaseMiddleware
 {
     public function handle($request, Closure $next)
     {
         try {
             $user = JWTAuth::parseToken()->authenticate();
-        } catch (JWTException $e) {
-            return response()->json([
-                'message' => 'ไม่สามารถตรวจสอบได้ กรุณา login ใหม่',
-                'status' => 0,
-                'code' => 'ER04',
-                'data' => null,
-            ], 401);
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['message' => 'Token is Invalid', 'status' => 0, 'code' => 'ER04']);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['message' => 'Token is Expired', 'status' => 0, 'code' => 'ER04']);
+            } else {
+                return response()->json(['message' => 'Authorization Token not found', 'status' => 0, 'code' => 'ER04']);
+            }
         }
-
         return $next($request);
     }
 }
