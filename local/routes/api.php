e9522d73e935942b239bd558782a32909b6e2347
sassanapong.p <sassanapong.p@gmail.com>
2024-05-15 13:09:35 +0700

    ddd

--- local/routes/api.php
+++ local/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ApiFunctionController;
 use App\Http\Controllers\Api\ApiFunction2Controller;
+use App\Http\Controllers\Api\ApiFunction3Controller;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,7 +29,7 @@ Route::get('dataset_changwat', [ApiFunctionController::class, 'dataset_changwat'
 Route::get('dataset_amphuress', [ApiFunctionController::class, 'dataset_amphuress'])->name('dataset_amphuress');
 Route::get('dataset_tambon', [ApiFunctionController::class, 'dataset_tambon'])->name('dataset_tambon');
 Route::post('storeRegister', [ApiFunction2Controller::class, 'storeRegister'])->name('storeRegister');
-Route::post('deposit', [ApiFunction2Controller::class, 'deposit'])->name('deposit');
+Route::post('deposit', [ApiFunction3Controller::class, 'deposit'])->name('deposit');
  
 // $id = Auth::guard('c_user')->user()->user_name;
 // $intoken = date("ymd") . '' . $id . '' . date("H");
