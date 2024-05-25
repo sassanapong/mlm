306f6044f4cb98cecbcafb9473ca8eca2e2677f1
sassanapong.p <sassanapong.p@gmail.com>
2024-05-25 13:08:59 +0700

    sss

--- local/app/Http/Controllers/Frontend/FC2024/NewUplineFunctionController.php
+++ local/app/Http/Controllers/Frontend/FC2024/NewUplineFunctionController.php
@@ -8,21 +8,21 @@ use DB;
 class NewUplineFunctionController extends Controller
 {
 
-    public static function check_all_upline()
+    public static function check_all_upline($username_check)
     {
 
         $upline_arr = []; // Initialize the array to store upline information
 
 
         $customers = DB::table('customers')
-            ->where('user_name', '0118983')
+            ->where('user_name', $username_check)
             ->orderByDesc('id')
             ->limit('1')
             ->get();
 
 
-        $i = 0;
         foreach ($customers as $introduce) {
+            $i = 0;
             $i++;
             if ($i == 1) {
                 $run_username = $introduce->user_name;
@@ -44,7 +44,8 @@ class NewUplineFunctionController extends Controller
                             'customers.expire_date',
                             'dataset_qualification.code',
                             'customers.introduce_id',
-                            'customers.upline_id'
+                            'customers.upline_id',
+                            'customers.type_upline',
                         )
                         ->leftJoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
                         ->where('customers.user_name', $user_name)
@@ -75,6 +76,8 @@ class NewUplineFunctionController extends Controller
                         'position' => $upline->qualification_id,
                         'introduce_id' => $upline->introduce_id,
                         'upline_id' => $upline->upline_id,
+                        'type_upline' => $upline->type_upline,
+
                         'g' => $level,
                     ];
 
@@ -96,18 +99,20 @@ class NewUplineFunctionController extends Controller
                             'customers.expire_date',
                             'dataset_qualification.code',
                             'customers.introduce_id',
-                            'customers.upline_id'
+                            'customers.upline_id',
+                            'customers.type_upline',
+
                         )
                         ->leftJoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
-                        ->where('customers.user_name', $user_name)
+                        ->where('customers.user_name', $run_username)
                         ->first();
-
 
 
                     return response()->json([
                         'status' => 'success',
                         'code' => 200,
-                        'data' => $upline_arr,
+                        'รหัสที่เช็ค' =>   $upline_check,
+                        'ข้อมูลผู้แนะนำทั้งสายงาน' =>   $upline_arr,
                         'message' => 'successfully',
                     ], 200);
                 } else {
@@ -122,26 +127,30 @@ class NewUplineFunctionController extends Controller
         }
     }
 
-
     public static function delete_empty_upline()
     {
-        $upline_arr = []; // Initialize the array to store upline information
+
 
         $customers = DB::table('customers')
-            ->where('user_name', '0118983')
             ->where('status_check_runupline', 'pending')
             ->orderByDesc('id')
-            ->limit(1)
+            ->limit(1000)
             ->get();
-        $i = 0;
+
+        $k = 0;
+        $delete = 0;
+
         foreach ($customers as $introduce) {
+            $i = 0;
             $i++;
+            $k++;
             if ($i == 1) {
                 $run_username = $introduce->user_name;
             }
             if ($introduce->introduce_id !== 'AA') {
                 $user_name = $introduce->introduce_id;
                 $status = 'fail';
+                $previous_user_name = null;
 
                 while (true) {
                     $upline = DB::table('customers')
@@ -156,10 +165,10 @@ class NewUplineFunctionController extends Controller
                             'dataset_qualification.code',
                             'customers.introduce_id',
                             'customers.upline_id',
-
+                            'customers.type_upline',
                         )
                         ->leftJoin('dataset_qualification', 'dataset_qualification.code', '=', 'customers.qualification_id')
-                        ->where('customers.user_name', '$run_username')
+                        ->where('customers.user_name', $user_name)
                         ->first();
 
                     // If no upline is found, exit the loop
@@ -168,15 +177,19 @@ class NewUplineFunctionController extends Controller
                     }
 
                     // Check if the upline has a valid name
-                    if (empty($upline->name)) {
+                    if (empty($upline->name) || $upline->name == '' || $upline->name == ' ') {
+                        $delete++;
                         // Save the introduce_id for the next iteration
                         $next_user_name = $upline->introduce_id;
                         $next_upline_id = $upline->upline_id;
+                        $next_type_upline = $upline->type_upline;
 
-                        // Update the introduce_id of the current customer to the user_name of the next valid upline
-                        DB::table('customers')
-                            ->where('user_name', $introduce->user_name)
-                            ->update(['introduce_id' => $next_user_name, 'upline_id' => $next_upline_id]);
+                        // Update the introduce_id of the previous customer to the user_name of the next valid upline
+                        if ($previous_user_name) {
+                            DB::table('customers')
+                                ->where('user_name', $previous_user_name)
+                                ->update(['introduce_id' => $next_user_name, 'upline_id' => $next_upline_id, 'type_upline' => $next_type_upline]);
+                        }
 
                         // Remove the upline with an empty name
                         DB::table('customers')->where('user_name', $user_name)->delete();
@@ -186,44 +199,21 @@ class NewUplineFunctionController extends Controller
                         continue;
                     }
 
-                    $full_name = "{$upline->name} {$upline->last_name}";
-
-                    // Append upline details to the array
-                    $upline_arr[] = [
-                        'username' => $upline->user_name,
-                        'name' => $full_name,
-                        'position' => $upline->qualification_id,
-                        'introduce_id' => $upline->introduce_id,
-                        'upline_id' => $upline->upline_id,
-
-                    ];
-
-                    // Set the user_name for the next level lookup
+                    $previous_user_name = $user_name; // Update previous_user_name before the next iteration
                     $user_name = $upline->introduce_id;
                     $status = 'success';
                 }
-
-                if ($upline_arr) {
-
-                    DB::table('customers')
-                        ->where('user_name', $run_username)
-                        ->update(['status_check_runupline' => 'success']);
-
-                    return response()->json([
-                        'status' => 'success',
-                        'code' => 200,
-                        'data' => $upline_arr,
-                        'message' => 'successfully',
-                    ], 200);
-                } else {
-                    return response()->json([
-                        'status' => 'success',
-                        'code' => 201,
-                        'data' => null,
-                        'message' => 'รหัสนี้ไม่มีผู้แนะนำ',
-                    ], 200);
-                }
             }
+
+            //รันเสร็จ
+            DB::table('customers')
+                ->where('user_name', $run_username)
+                ->update(['status_check_runupline' => 'success']);
         }
+
+        $pending = DB::table('customers')
+            ->where('status_check_runupline', 'pending')
+            ->count();
+        dd('รันไปทั้งหมด:' . $k, 'ลบรหัสไป:' . $delete, 'รหัสรอดำเนินการ:' . $pending, 'success');
     }
 }
