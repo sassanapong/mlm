5706719024a8a53ea2126f9f55ab35d5cdf81b5f
sassanapong.p <sassanapong.p@gmail.com>
2024-06-03 15:33:03 +0700

    sss

--- local/app/Http/Controllers/Frontend/RegisterController.php
+++ local/app/Http/Controllers/Frontend/RegisterController.php
@@ -344,7 +344,7 @@ class RegisterController extends Controller
 
             $code_bonus =  \App\Http\Controllers\Frontend\FC\RunCodeController::db_code_bonus(2);
 
-            for ($i = 1; $i <= 10; $i++) {
+            for ($i = 1; $i <= 8; $i++) {
                 $x = 'start';
                 $data_user =  DB::table('customers')
                     ->select('customers.name', 'customers.last_name', 'customers.user_name', 'customers.introduce_id', 'customers.qualification_id', 'customers.expire_date')
@@ -393,11 +393,11 @@ class RegisterController extends Controller
                             $arr_user[$i]['user_name'] = $data_user->user_name;
                             $arr_user[$i]['lv'] = [$i];
                             if ($i == 1) {
-                                $report_bonus_register[$i]['percen'] = 250;
+                                $report_bonus_register[$i]['percen'] = 100;
 
                                 $arr_user[$i]['pv'] = $pv_register;
                                 $arr_user[$i]['position'] = $qualification_id;
-                                $wallet_total = $pv_register * 250 / 100;
+                                $wallet_total = $pv_register * 100 / 100;
                                 $arr_user[$i]['bonus'] = $wallet_total;
                                 $report_bonus_register[$i]['tax_total'] = $wallet_total * 3 / 100;
                                 $report_bonus_register[$i]['bonus_full'] = $wallet_total;
@@ -421,7 +421,7 @@ class RegisterController extends Controller
                                 $report_bonus_register[$i]['percen'] = 10;
                                 $arr_user[$i]['pv'] = $pv_register;
                                 $arr_user[$i]['position'] = $qualification_id;
-                                if ($qualification_id == 'MB') {
+                                if ($qualification_id == 'MB' || $qualification_id == 'MO') {
                                     $report_bonus_register[$i]['tax_total'] = 0;
                                     $report_bonus_register[$i]['bonus_full'] = 0;
                                     $report_bonus_register[$i]['bonus'] = 0;
@@ -439,7 +439,7 @@ class RegisterController extends Controller
                                 $arr_user[$i]['pv'] = $pv_register;
                                 $arr_user[$i]['position'] = $qualification_id;
 
-                                if ($qualification_id == 'MB' || $qualification_id == 'MO') {
+                                if ($qualification_id == 'MB' || $qualification_id == 'MO' ||  $qualification_id == 'VIP') {
                                     $report_bonus_register[$i]['tax_total'] = 0;
                                     $report_bonus_register[$i]['bonus_full'] = 0;
                                     $report_bonus_register[$i]['bonus'] = 0;
@@ -452,18 +452,12 @@ class RegisterController extends Controller
                                     $report_bonus_register[$i]['bonus_full'] = $wallet_total;
                                     $report_bonus_register[$i]['bonus'] = $wallet_total - $wallet_total * 3 / 100;
                                 }
-                            } elseif ($i >= 5 and $i <= 10) {
+                            } elseif ($i >= 5 and $i <= 8) {
                                 $report_bonus_register[$i]['percen'] = 5;
                                 $arr_user[$i]['pv'] = $pv_register;
                                 $arr_user[$i]['position'] = $qualification_id;
 
-                                if (($i == 5 || $i == 6 || $i == 7) and $qualification_id == 'MB' || $qualification_id == 'MO' || $qualification_id == 'VIP') {
-                                    $report_bonus_register[$i]['tax_total'] = 0;
-                                    $report_bonus_register[$i]['bonus_full'] = 0;
-                                    $report_bonus_register[$i]['bonus'] = 0;
-                                    $arr_user[$i]['bonus'] = 0;
-                                } elseif (($i == 8 || $i == 9 || $i == 10) and $qualification_id == 'MB' || $qualification_id == 'MO' || $qualification_id == 'VIP' || $qualification_id == 'VVIP') {
-
+                                if ($qualification_id == 'MB' || $qualification_id == 'MO' ||  $qualification_id == 'VIP') {
                                     $report_bonus_register[$i]['tax_total'] = 0;
                                     $report_bonus_register[$i]['bonus_full'] = 0;
                                     $report_bonus_register[$i]['bonus'] = 0;
