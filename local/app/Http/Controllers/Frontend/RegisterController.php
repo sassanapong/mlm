fe718a4a60aaf36c4fdce275248eda73f7dc781f
sassanapong.p <sassanapong.p@gmail.com>
2024-06-17 17:56:57 +0700

    dddd

--- local/app/Http/Controllers/Frontend/RegisterController.php
+++ local/app/Http/Controllers/Frontend/RegisterController.php
@@ -748,7 +748,7 @@ class RegisterController extends Controller
                         }
                     }
 
-                    self::up_lv($request->sponser);
+                    // self::up_lv($request->sponser);
 
                     //คำนวนตำแหน่งไหม่
 
